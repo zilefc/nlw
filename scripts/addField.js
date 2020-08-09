1cb15572ce69e9031129ebc92b2892302a641a08
zilefc <fczile@gmail.com>
2020-08-09 17:24:53 +0200

    files

--- scripts/addField.js
+++ scripts/addField.js
@@ -3,8 +3,17 @@ document.querySelector("#add-time")
 .addEventListener('click', cloneField)
 //quando clicar executar uma accao 
 function cloneField() {
-    console.log("ck")
+ //duplicar os campos. que campos?
+ const newFieldContainer=document.querySelector('.schedule-item').cloneNode(true)
+ //limpar os campos. que campos?
+ const fields =newFieldContainer.querySelectorAll('input')
+
+fields.forEach(function(field){
+    field.value = ""
+})
+
+//colocar na pagina, onde?
+document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
-  //duplicar
-    //colocar na pagina
+ 
 
