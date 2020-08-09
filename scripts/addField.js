4e7c0e386ab5536b930aedb09422fb3fe9c0fc68
zilefc <fczile@gmail.com>
2020-08-09 16:49:33 +0200

    files

---
+++ scripts/addField.js
@@ -0,0 +1,10 @@
+//procurar botao
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+//quando clicar executar uma accao 
+function cloneField() {
+    console.log("ck")
+}
+  //duplicar
+    //colocar na pagina
+
